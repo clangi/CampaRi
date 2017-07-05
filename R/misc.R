b1550a5e28806d9f23f3dc4896efaf0e7ee01abe
Melkiades <dgarolini@gmail.com>
2017-07-05 17:04:03 +0200

    testing that

--- R/misc.R
+++ R/misc.R
@@ -14,31 +14,31 @@
     " ==============================================================\n"))
 }
 
-.onLoad <- function(libname, pkgname) {
-  op <- options()
-  op.CampaRi <- list(
-    CampaRi.data_management = "R"
-  )
-  toset <- !(names(op.CampaRi) %in% names(op))
-  if(any(toset)) options(op.CampaRi[toset])
-  # .setting_up_netcdf()
-#   if(getOption("CampaRi.data_management")=='netcdf'){
-#     nc_lib_dir <- "/usr/include/"
-#     makevars_file <- paste0('MY_PKG_LIBS= -lnetcdff -I', nc_lib_dir, '
-# MY_PKG_FFLAGS= -fbacktrace -fbounds-check -fcheck-array-temporaries -g
-# mypackage_FFLAGS = $(FPICFLAGS) $(SHLIB_FFLAGS) $(FFLAGS)
-# all: $(SHLIB)
-# main_clu_adjl_mst.o: main_clu_adjl_mst.f90
-#         $(FC) $(mypackage_FFLAGS) $(MY_PKG_FFLAGS) -c main_clu_adjl_mst.f90 -o main_clu_adjl_mst.o $(MY_PKG_LIBS)
-# 
-# utilities_netcdf.o: utilities_netcdf.f90
-#         $(FC) $(mypackage_FFLAGS) $(MY_PKG_FFLAGS) -c utilities_netcdf.f90 -o utilities_netcdf.o $(MY_PKG_LIBS)
-# 
-# PKG_LIBS= -lnetcdff -I', nc_lib_dir, '
-# ')
-#     cat(makevars_file, file = paste0(".R/Makevars"))
-#   }
-  invisible() #no output from this function
-}
+# .onLoad <- function(libname, pkgname) {
+#   op <- options()
+#   op.CampaRi <- list(
+#     CampaRi.data_management = "R"
+#   )
+#   toset <- !(names(op.CampaRi) %in% names(op))
+#   if(any(toset)) options(op.CampaRi[toset])
+#   # .setting_up_netcdf()
+# #   if(getOption("CampaRi.data_management")=='netcdf'){
+# #     nc_lib_dir <- "/usr/include/"
+# #     makevars_file <- paste0('MY_PKG_LIBS= -lnetcdff -I', nc_lib_dir, '
+# # MY_PKG_FFLAGS= -fbacktrace -fbounds-check -fcheck-array-temporaries -g
+# # mypackage_FFLAGS = $(FPICFLAGS) $(SHLIB_FFLAGS) $(FFLAGS)
+# # all: $(SHLIB)
+# # main_clu_adjl_mst.o: main_clu_adjl_mst.f90
+# #         $(FC) $(mypackage_FFLAGS) $(MY_PKG_FFLAGS) -c main_clu_adjl_mst.f90 -o main_clu_adjl_mst.o $(MY_PKG_LIBS)
+# # 
+# # utilities_netcdf.o: utilities_netcdf.f90
+# #         $(FC) $(mypackage_FFLAGS) $(MY_PKG_FFLAGS) -c utilities_netcdf.f90 -o utilities_netcdf.o $(MY_PKG_LIBS)
+# # 
+# # PKG_LIBS= -lnetcdff -I', nc_lib_dir, '
+# # ')
+# #     cat(makevars_file, file = paste0(".R/Makevars"))
+# #   }
+#   invisible() #no output from this function
+# }
 
 .lt <- function(x) return(length(x))
