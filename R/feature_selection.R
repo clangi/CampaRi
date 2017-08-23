57ee0cf7b2f2acb18d9529597509986f22ee4eb5
Melkiades <dgarolini@gmail.com>
2017-08-23 12:27:43 +0200

    adding labels and legends to feature_selection

--- R/feature_selection.R
+++ R/feature_selection.R
@@ -30,11 +30,11 @@
 #' @importFrom stats loadings
 #' @importFrom plotly ggplotly
 #' @import ggplot2
-#' 
+#' @import ggfortify
 
 select_features <- function(trj, feature_selection = 'pca', n_princ_comp = floor(ncol(trj)/10), pca_method = 'R',
                             plotit = FALSE, frameit = FALSE, return_plot = FALSE, cluster_vector = NULL, plotly_it = FALSE,
-                            points_size = 1){
+                            points_size = 1, specific_palette = NULL, plot_legend = FALSE, legend_title = NULL, legend_labels = NULL){
 
   # Checking input variables (again - it is also a stand alone function)
   if(is.null(n_princ_comp) || (length(n_princ_comp) != 1 || !is.numeric(n_princ_comp) || n_princ_comp < 0 || n_princ_comp > ncol(trj)))
@@ -46,22 +46,14 @@ select_features <- function(trj, feature_selection = 'pca', n_princ_comp = floor
   if(is.null(pca_method) || (length(pca_method) != 1 ||!is.character(pca_method) || !(pca_method %in% c('robust','R'))))
     stop('The inserted pca method is not correctly defined.')
   
-  # checking the cluster vector
-  if(!is.null(cluster_vector)){
-    if(length(cluster_vector) != nrow(trj))
-      stop('The cluster vector length is different from the number of snapshots.')
-    if(!is.numeric(cluster_vector))
-      stop('The cluster vector must be numeric.')
-    if(any(cluster_vector %% 1 != 0))
-      stop('Only integers can be inserted.')
-    col <- 'clusters'
-  }else{
-    col <- NULL
-  }
-  
   # Checking the size of the points
   if(!is.numeric(points_size) || length(points_size) != 1)
     stop('The points_size must be a single numeric.')
+  
+  # checking the palette
+  if(!is.null(specific_palette) &&
+     (!all(is.character(specific_palette)) || !all(nchar(specific_palette)==7) || !all(sapply(specific_palette, function(x){substring(x,1,1)}) == "#")))
+    stop('specific_palette must be of the type "#b47b00", "#D9E000" (7 characters starting with #')
   
   # Checking the logicals
   if(!is.logical(plotit))
@@ -72,6 +64,82 @@ select_features <- function(trj, feature_selection = 'pca', n_princ_comp = floor
     stop('return_plot must be a logical value.')
   if(!is.logical(plotly_it))
     stop('plotly_it must be a logical value.')
+  
+  # adding the cross checks
+  if(plotit){
+    # checking the cluster vector
+    if(!is.null(cluster_vector)){
+      if(length(cluster_vector) != nrow(trj))
+        stop('The cluster vector length is different from the number of snapshots.')
+      if(!is.numeric(cluster_vector))
+        stop('The cluster vector must be numeric.')
+      if(any(cluster_vector %% 1 != 0))
+        stop('Only integers can be inserted.')
+      col <- 'clusters'
+    }else{
+      col <- NULL
+      cluster_vector <- rep(1, nrow(trj))
+    }
+    if(!plot_legend && (!is.null(legend_title) || !is.null(legend_labels))){
+      warning('Inserted legend_title or legend_labels or annotation_type variables WITHOUT activating plot_legend. This option will be turned on automatically.')
+      plot_legend <- TRUE
+    }
+    
+    # checking the legend
+    if(plot_legend){
+      # checking the legend title
+      if(!is.null(legend_title)){
+        if(!is.character(legend_title))
+          stop('legend_title must be a character.')
+        if(length(legend_title) != 1)
+          stop('legend_title must be a SINGLE character.')
+        
+        leg_tit <- legend_title
+      }else{
+        leg_tit <- 'Legend'
+      }
+      # checking the labels
+      if(!is.null(legend_labels)){
+        if(!is.character(legend_labels))
+          stop('legend_labels must be a vector of characters.')
+        if(!is.null(dim(legend_labels)))
+          stop('legend_labels must be a vector of characters (found more than 1 dimension).')
+        leg_lab <- c(legend_labels)
+      }else{
+        warning('No label inserted for the legend. they will be assigned to integers.')
+        leg_lab <- NULL
+      }
+      # checking the legend vs the palette etc
+      cluster_vector <- factor(cluster_vector)
+      if(!is.null(specific_palette)){
+        if(length(specific_palette) != length(levels(cluster_vector)))
+          stop('When using a specific palette please set it to the same number of factors in the cluster_vector.')
+      }else{
+        specific_palette <- levels(cluster_vector)
+      }
+      if(is.null(leg_lab)) leg_lab <- seq(1, length(specific_palette))
+      if(!is.null(leg_lab) && length(leg_lab) != length(specific_palette))
+        stop('Inserted different number of labels than colors. In this mode this cannot be done.')
+      if(!is.null(leg_lab) && length(leg_lab) != length(levels(cluster_vector)))
+        stop('Inserted different number of labels than factors in the cluster_vectors (e.g. different numbers 1-2-3).') 
+      if(length(levels(cluster_vector)) > 10)
+        warning('There are a lot of levels (clusters > 10).')
+      if(length(levels(cluster_vector)) > 20){
+        stop('There are too many levels (clusters > 20).')
+      }
+    }
+  }else{
+    if(!is.null(cluster_vector))
+      stop('Please put plotit variables TRUE if you want to define the clusters to plot.')
+    if(frameit)
+      stop('Please put plotit variables TRUE if you want to frame the clusters to plot.')
+    if(plotly_it)
+      stop('Please put plotit variables TRUE if you want to plot the pca using plotly.')
+    if(return_plot)
+      stop('Please put plotit variables TRUE if you want to return the plot object.')
+    if(plot_legend || !is.null(legend_title) || !is.null(specific_palette) || !is.null(legend_labels))
+      stop('You inserted something between plot_legend, legend_title, specific_palette, legend_labels but you did not activate the plotting.')
+  }
   
   # General messages
   cat('Feature selection mode active.', feature_selection, 'dimensionality reduction will be performed.\n')
@@ -100,8 +168,19 @@ select_features <- function(trj, feature_selection = 'pca', n_princ_comp = floor
     if(any(selected_components == 0)) stop('An internal error occured. Please refer it to the mantainers.')
   
     # plotting it eventually
-    if(plotit) appca <- autoplot(pcahah, data = cbind(trj, clusters = cluster_vector), colour = col, size=0.1*points_size, frame=frameit) + theme_minimal()
-    if(return_plot) appca <- autoplot(pcahah, data = cbind(trj, clusters = cluster_vector), colour = col, size=0.1*points_size, frame=frameit) + theme_minimal()
+    if(plotit || return_plot){
+      appca <- autoplot(pcahah, data = cbind(trj, clusters = cluster_vector), colour = col, size=0.1*points_size, frame=frameit) + theme_minimal()
+      # plot the legend
+      if(plot_legend){
+        appca <- appca + scale_color_manual(name = leg_tit, values = specific_palette, labels = leg_lab) + 
+          guides(color = guide_legend(override.aes = list(size=5)))
+      }else{
+        if(!is.null(specific_palette))
+          appca <- appca + scale_color_gradientn(colours = specific_palette, guide = FALSE)
+        else
+          appca <- appca + theme(legend.position='none')
+      }
+    } 
     if(plotly_it) {
       if(nrow(trj) > 15000)
         warning('nrow(trj) is more than 15000. Ggplotly would be stupidly slow and crash. Consider reducing the dimensionality. It will plotted normally.')
@@ -114,12 +193,16 @@ select_features <- function(trj, feature_selection = 'pca', n_princ_comp = floor
     message('The details of the pca output will be oscurated but the selected values will be written in "selected_pca.out" ASCII file.')
     fwrite(x = as.list(selected_components), file = "selected_pca.out", sep = '\t')
   }
-  if(plotit) plot(appca)
+  
   if(return_plot){
     cat('Returning the plot object. No selected features will be returned with this mode active.\n')  
     invisible(appca)
   }else{
-    cat('Returning the selected features...\n')  
-    return(trj[,selected_components])
+    if(plotit) print(appca)
+    cat('Returning the selected features...\n')
+    if(!plotit)
+      return(trj[,selected_components])
+    else
+      invisible(trj[,selected_components])
   }
 }
