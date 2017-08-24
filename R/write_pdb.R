eb9faadcac52614d49cd55a847b60f8794a8ebbe
Melkiades <dgarolini@gmail.com>
2017-08-24 18:17:52 +0200

    testing writing pdb completed

--- R/write_pdb.R
+++ R/write_pdb.R
@@ -24,6 +24,7 @@
 
 write_pdb<-function(trj, file_name, seq_name=NULL, method = "automatic_safe", round=FALSE, digit=4, dim_check = TRUE, ...){
   # input checks
+  complete_table_atom_specification <- FALSE
   if(! method %in% c('bio3d', 'automatic_safe', 'automatic_unsafe'))
     stop('not supported methods')
   
@@ -37,29 +38,58 @@ write_pdb<-function(trj, file_name, seq_name=NULL, method = "automatic_safe", ro
     
   if(!is.matrix(trj)){
     if(!is.data.frame(trj)) stop('trj input must be a matrix or a data.frame')
-    trj <- as.matrix(trj)
-    if(!is.numeric(trj)) stop("Numeric input must be supplied.")
+    if(trj[1,1] != "ATOM"){
+      trj <- as.matrix(trj)
+      if(ncol(trj)%%3 != 0)
+        stop('The trj matrix must be a multiple of 3 to write a pdb file correctly formatted (even if it is not molecular data).')
+      if(!is.numeric(trj)) stop("Numeric input must be supplied for only coordinates inputs.")
+      cat('You inserted only numeric values. We suppose they are the tridimensional objects to write. Not knowing the AA type CL CL- will be used in place.\n')
+    }else if(trj[1,1] == "ATOM"){
+      cat('You inserted a structured data-set. Please consider that they must be structured in the following way:\n
+  V1 V2     V3     V4 V5   V6   V7    V8    V9    V10  V11\n
+ATOM  1    CH3    ACE  A    1 30.11 46.91 45.81     1    0\n
+ATOM  2      C    ACE  A    1 31.33 46.73 46.64     1    0\n
+ATOM  3      O    ACE  A    1 32.07 45.75 46.56     1    0\n
+ATOM  4     1H    ACE  A    1 29.28 46.28 46.20     1    0\n
+ATOM  5     2H    ACE  A    1 29.84 47.98 45.98     1    0\n
+ATOM  6     3H    ACE  A    1 30.35 46.85 44.73     1    0\n')
+      complete_table_atom_specification <- TRUE
+    }else{
+      stop('Not supported structure! the first trj[1,1] must be == "ATOM" with the following structure or the trj must be a coordinate trj.
+  V1 V2     V3     V4 V5   V6   V7    V8    V9    V10  V11\n
+ATOM  1    CH3    ACE  A    1 30.11 46.91 45.81     1    0\n
+ATOM  2      C    ACE  A    1 31.33 46.73 46.64     1    0\n
+ATOM  3      O    ACE  A    1 32.07 45.75 46.56     1    0\n
+ATOM  4     1H    ACE  A    1 29.28 46.28 46.20     1    0\n
+ATOM  5     2H    ACE  A    1 29.84 47.98 45.98     1    0\n
+ATOM  6     3H    ACE  A    1 30.35 46.85 44.73     1    0\n           
+')
+    }
   }
-  if(ncol(trj)%%3 != 0)
-    stop('The trj matrix must be a multiple of 3 to write a pdb file correctly formatted (even if it is not molecular data).')
-  
+ 
   # main functions
   if(method == 'bio3d'){
     message('Selected bio3d handling of PBD. Please check the documentation of bio3d::write.pdb and add all the necessary variables to this function (or use it directly')
     for(i in 1:nrow(trj))
       bio3d::write.pdb(file = file_name, xyz = trj[i,], append = TRUE, ...) # todo
   }else if(method == 'automatic_safe'){
-    message('Selected sprintf support! it will be printed to file in append using a general scheme with CL CL- types. For more specifications please use the bio3d option.')
-    pdbfile <- file(file_name, open="w")
-    for(u in 1:nrow(trj)){
-      sst <- sprintf("MODEL %8i\n", as.integer(u))
-      sst2 <- NULL
-      for(u2 in 1:(ncol(trj)/3))
-        sst2 <- paste0(sst2, sprintf("ATOM  %5i  CL  CL- %5i    %8.3f%8.3f%8.3f\n", as.integer(u2), as.integer(u2), trj[u, u2], trj[u, u2 + 1], trj[u, u2 + 2]))
-      write(paste0(sst, sst2, "ENDMDL"), file=pdbfile, append = TRUE)
+    if(!complete_table_atom_specification){
+      pdbfile <- file(file_name, open="w")
+      message('Selected sprintf support! it will be printed to file in append using a general scheme with CL CL- types.')
+      for(u in 1:nrow(trj)){
+        sst <- sprintf("MODEL %8i\n", as.integer(u))
+        sst2 <- NULL
+        for(u2 in 1:(ncol(trj)/3))
+          sst2 <- paste0(sst2, sprintf("ATOM  %5i  CL  CL- %5i    %8.3f%8.3f%8.3f\n", as.integer(u2), as.integer(u2), trj[u, u2], trj[u, u2 + 1], trj[u, u2 + 2]))
+        write(paste0(sst, sst2, "ENDMDL"), file=pdbfile, append = TRUE)
+      } 
+      close(pdbfile)
+    }else{
+      message('Selected sprintf support! The printing format will be ATOM  1 CH3    ACE  A    1 30.11 46.91 45.81        1     0.')
+      write.table(sprintf("%-6s %4d  %-4s%3s %1s %3d     %7.3f %7.3f %7.3f  %1d %1d",
+                          trj[,1], trj[,2], trj[,3], trj[,4], trj[,5], trj[,6], trj[,7], trj[,8], trj[,9], trj[,10], trj[,11]), 
+                  file_name, col.names = FALSE, row.names = FALSE, quote = FALSE)
     }
-    close(pdbfile)
-    
   }else if(method == 'automatic_unsafe'){
     if(file.exists(file_name)) system(paste0("rm ", file_name))
     if(file.exists(seq_name)) system(paste0("rm ", seq_name))
---
+++ tests/testthat/test_writing_pdb.R
@@ -0,0 +1,20 @@
+context('writing a pdb')
+
+
+test_that('writing a pdb', {
+  data_f <- matrix(rnorm(n = 1000), nrow = 100, ncol = 10)
+  expect_that(write_pdb(trj = data_f, file_name = "acciaio_inox.pdb"), not(throws_error()))
+  expect_that(write_pdb(trj = data_f, file_name = "acciaio_inox.pdb", seq_name = "seq.in"), not(throws_error()))
+  expect_that(write_pdb(trj = data_f[,1:9], file_name = "acciaio_inox.pdb", method = "bio3d"), not(throws_error()))
+  expect_that(write_pdb(trj = data_f[,1:9], file_name = "acciaio_inox.pdb", method = "automatic_unsafe"), not(throws_error()))
+  expect_that(write_pdb(trj = data_f, file_name = "acciaio_inox.pdb", method = "automatic_safe", dim_check = T), not(throws_error()))
+  data_f <- 'ATOM  1    CH3    ACE  A    1 30.11 46.91 45.81     1    0
+ATOM  2      C    ACE  A    1 31.33 46.73 46.64     1    0
+ATOM  3      O    ACE  A    1 32.07 45.75 46.56     1    0
+ATOM  4     1H    ACE  A    1 29.28 46.28 46.20     1    0
+ATOM  5     2H    ACE  A    1 29.84 47.98 45.98     1    0
+ATOM  6     3H    ACE  A    1 30.35 46.85 44.73     1    0'
+  library(data.table)
+  data_f <- fread(data_f, data.table = F)
+  expect_that(write_pdb(trj = data_f, file_name = "acciaio_inox.pdb", method = "automatic_safe", dim_check = F), not(throws_error()))
+})
